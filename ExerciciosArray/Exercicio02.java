ebcbdd0125f584e8a0ba0d68ad7f8a996c314efb
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2025-03-11 15:14:20 -0300

    Exercicios de Array e ArrayList

---
+++ ExerciciosArray/Exercicio02.java
@@ -0,0 +1,53 @@
+package ExerciciosArray;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.print("Quantas palavras deseja inserir? ");
+        int n = leia.nextInt();
+        leia.nextLine(); 
+        
+        String[] palavras = new String[n];
+        String[] palindromos = new String[n];
+        int countPalindromos = 0;
+        
+      
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite a palavra " + (i + 1) + ": ");
+            palavras[i] = leia.nextLine();
+        }
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (ehPalindromo(palavras[i])) {
+                palindromos[countPalindromos++] = palavras[i];
+            }
+        }
+        
+        
+        System.out.println("Palavras palíndromas:");
+        for (int i = 0; i < countPalindromos; i++) {
+            System.out.println(palindromos[i]);
+        }
+        
+        leia.close();
+    }
+
+    
+    public static boolean ehPalindromo(String palavra) {
+        int esquerda = 0;
+        int direita = palavra.length() - 1;
+        
+        while (esquerda < direita) {
+            if (palavra.charAt(esquerda) != palavra.charAt(direita)) {
+                return false;
+            }
+            esquerda++;
+            direita--;
+        }
+        return true;
+    }
+}
---
+++ ExerciciosArray/Exercicio04.java
@@ -0,0 +1,38 @@
+package ExerciciosArray;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        ArrayList<String> contatos = new ArrayList<>();
+
+        System.out.println("Digite a quantidade de contatos para casdastrar: ");
+        int qtd = leia.nextInt();
+        leia.nextLine();
+
+        for(int i = 0; i < qtd; i++){
+            System.out.println("Digite o nome do contato: ");
+            String nome = leia.nextLine();
+            System.out.println("Digite o telefone do contato: ");
+            String telefone = leia.nextLine();
+            contatos.add(nome  + " - " + telefone);
+        }
+
+        System.out.println("Digite um nome para buscar: ");
+        String buscaNome = leia.nextLine();
+        
+       System.out.println(contatos.contains(buscaNome) ? "Contato encontrado!" : "Contato não encontrado!");
+
+        System.out.println("");
+        System.out.println("Lista de contatos: ");
+
+        for(int i = 0; i < contatos.size(); i++){
+            System.out.println(contatos.get(i));
+        }
+
+        leia.close();
+    }
+}
